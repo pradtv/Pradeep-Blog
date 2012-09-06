db00947ee51b7b0f9c78d249107300dee92be889
PRADEEP TV <pradeep@wowmakers.com>
2012-09-06 16:39:34 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'fe4c502120c05e2fffa50166fe3700d314bf82dd8e5963f969d264cddf1497523273f582d72e7161d6402ed5912cb2215d3b21a5364d98a3b19fe8aee41dd48f'
